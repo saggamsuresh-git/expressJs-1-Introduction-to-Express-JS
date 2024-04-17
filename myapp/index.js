738b1e0c1192718b2ae2e45af265dbe6eef3cc57
root <saggamsuresh3@gmail.com>
2024-04-17 11:35:02 +0000

    intro to expressJS

---
+++ myapp/index.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+// const app = express();
+// app.get("/", (request, response) => {
+//   response.send("Hello World!");
+//   //   console.log(request);
+// });
+// app.listen(3000);
+
+const express = require("express");
+const app = express();
+app.get("/date", (request, response) => {
+  let date = new Date();
+  response.send(`Today's date is ${date}`);
+});
+
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
+app.listen(3000);
